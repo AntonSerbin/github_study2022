1d8b399d8f5a403a25f2e46fe6542644968d57da
AntonSerbin <antons.zn@gmail.com>
2022-07-03 14:12:50 +0300

    to MVC branch

--- index.php
+++ index.php
@@ -1,13 +1,38 @@
 ﻿<?php
+
+use App\Models\Session;
+
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-require_once 'Models/session.php';
-?>
+require_once "vendor/autoload.php";
+//require_once 'Models/Session.php';
+//<form action="src/Controllers/login.php" method="post">
+//    Name: <input type="text" name="name"><br>
+//    E-mail: <input type="text" name="email"><br>
+//    Password: <input type="password" name="pswd"><br>
+//<input type="submit">
+//</form>
 
-<form action="./Controllers/login.php" method="post">
-    Name: <input type="text" name="name"><br>
-    E-mail: <input type="text" name="email"><br>
-    Password: <input type="password" name="pswd"><br>
-<input type="submit">
-</form>
+
+echo "Login Page<br>";
+
+echo "<pre> <b>Posted information from login page</b><br><br>";
+print_r($_POST);
+echo "</pre>";
+
+
+$sessionNew = new Session();
+
+if ($_POST["name"] != "" || $_POST["email"] != "") {
+    $sessionNew->resetSession();
+    $sessionNew->addSession("name", $_POST["name"]);
+    $sessionNew->addSession("email", $_POST["email"]);
+    $hashed_password = password_hash($_POST["pswd"], PASSWORD_DEFAULT);
+    $sessionNew->addSession("pswd", $hashed_password);
+} else
+    echo "<br/>There is not enough information to change Session";
+
+$sessionNew->showSession();
+
+echo "<button onclick='window.location.href=`../../index_HW_Session.php`'>Return</button>";
 
--- src/Models/Session.php
+++ src/Models/Session.php
@@ -1,34 +1,42 @@
 <?php
-class sessionControl
+
+namespace App\Models;
+
+class Session
 {
-    function __construct() {
+    public function __construct()
+    {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
     }
+
     public function addSession($index, $value)
     {
         $_SESSION[$index] = $value;
         return $_SESSION[$index];
     }
+
     public function showSession()
     {
         echo "<pre> <b>Information in current session</b><br><br>";
-        echo "ID of session:".session_id()."<br/>";
+        echo "ID of session:" . session_id() . "<br/>";
         print_r($_SESSION);
         echo "</pre>";
     }
-    public function getSession(){
-        return$_SESSION;
+
+    public function getSession()
+    {
+        return $_SESSION;
     }
+
     public function resetSession()
     {
-        $_SESSION=[];
+        $_SESSION = [];
     }
-
 }
 
-$sessionNew = new sessionControl;
+//$sessionNew = new Session();
 
 //$sessionNew->addSession("name1", "value1");
 //$sessionNew->addSession("name2", "value2");
